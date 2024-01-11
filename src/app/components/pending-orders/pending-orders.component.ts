f17474d3d08d35a1b396c57f257f3f88904bac9b
Natalia392 <ntorrejonvalencia@gmail.com>
2024-01-10 21:30:11 -0300

    feat: create function to calculate elapsed time for order This function only calculates the elapsed time, but it may be better to create a timer

--- src/app/components/pending-orders/pending-orders.component.ts
+++ src/app/components/pending-orders/pending-orders.component.ts
@@ -9,14 +9,22 @@ import { OrderProductService } from 'src/app/services/orderProduct.service';
   styleUrls: ['./pending-orders.component.css']
 })
 export class PendingOrdersComponent implements OnInit {
-  pendingOrders: RPOrder[] = [];
+
   @Output() orderReady = new EventEmitter<RPOrder>();
+
+  pendingOrders: RPOrder[] = [];
+
+  elapsedMinutes = 0;
 
   constructor(private ordersService: OrderProductService) {}
 
   ngOnInit(): void {
     this.showPendingOrders();
     this.updateOrdersView();
+  }
+
+  showItem(a: RPOrder) {
+    console.log(a);
   }
 
   showPendingOrders() {
@@ -26,7 +34,6 @@ export class PendingOrdersComponent implements OnInit {
   }
 
   updateOrdersView(): void {
-    // this.addNewOrders();
     this.pendingOrders = this.pendingOrders.slice().sort((a, b) => {
       return new Date(b.dateEntry).getTime() - new Date(a.dateEntry).getTime();
     });
@@ -35,10 +42,20 @@ export class PendingOrdersComponent implements OnInit {
   markAsReady(id: number) {
     const index = this.pendingOrders.findIndex(item => item.id === id);
     if (index !== -1) {
+      // obtiene el tiempo inicial (el momento en el que el pedido ingresó)
+      const startTime = new Date(this.pendingOrders[index].dateEntry);
+
       this.ordersService.processOrder(id).subscribe((data) => {
+        // Obtiene el tiempo en el que se marca como ready, que sería el endTime de preparación
+        const endTime = new Date();
+        // Se calcula el tiempo transcurrido desde que ingresó hasta que se preparó
+        const elapsedTime = endTime.getTime() - startTime.getTime();
+        const elapsedSeconds = Math.floor(elapsedTime / 1000);
+        this.elapsedMinutes = Math.floor(elapsedSeconds / 60);
+
         this.pendingOrders.splice(index, 1);
         this.orderReady.emit(data);
-        console.log(data);
+        console.log(data, this.elapsedMinutes);
       })
     }
   }
